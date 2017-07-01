48e6464a875487765ef5b9e68b9124ce518c896a
skmbw <tongku2008@126.com>
2017-07-01 16:16:30 +0800

    注释

--- main.cpp
+++ main.cpp
@@ -6,16 +6,21 @@ using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     // 不要使用char* 去声明一个字符串，https://zhidao.baidu.com/question/534987671.html
-    // 要用的話，也要使用const char * ab = "yinlei尹雷";
+    // 要用的话，也要使用const char * ab = "yinlei尹雷";
     char ab[] = "yinlei尹雷";
     cout << ab << endl;
     string s = "yinqianxun尹千寻";
-    printf("c style %s", ab); // 在stdio.h中
+    // 'printf'在此作用域中尚未声明，见
+    // https://zhidao.baidu.com/question/390847449618643645.html
+    printf("c style printf %s\n", ab); // 在stdio.h中
+
     // char数组就是字符串
     string s2 = ab;
     const char * s3 = s2.c_str();
     cout << s << endl;
     cout << s2 << endl;
     cout << s3 << endl;
+    unsigned ui = -1;
+    cout << ui << endl;
     return 0;
 }
