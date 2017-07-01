a8fc10eeb0145a68fb68fe7fdec4d2d1d458931c
skmbw <tongku2008@126.com>
2017-07-01 20:30:32 +0800

    格式化代码

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main() {
 
     // char数组就是字符串
     string s2 = ab;
-    const char * s3 = s2.c_str();
+    const char *s3 = s2.c_str();
     cout << s << endl;
     cout << s2 << endl;
     cout << s3 << endl;
@@ -30,24 +30,19 @@ int main() {
     Mat image = imread("/home/yinlei/miao.png");
     Mat image1(image.rows, image.cols, image.type(), Scalar(180, 120, 50));
     Mat image2(image.rows, image.cols, image.type(), Scalar(180, 120, 50));
-    Point center(image.cols/2,image.rows/2);
+    Point center(image.cols / 2, image.rows / 2);
     int radius = 200;
 
     circle(image, center, radius, Scalar(0, 200, 100), 2, 8, 0);
 
-    for (int x = 0; x < image.cols; x++)
-    {
-        for (int y = 0; y < image.rows; y++)
-        {
-            int temp = ((x - center.x) * (x - center.x) + (y - center.y) *(y - center.y));
-            if (temp < (radius * radius))
-            {
+    for (int x = 0; x < image.cols; x++) {
+        for (int y = 0; y < image.rows; y++) {
+            int temp = ((x - center.x) * (x - center.x) + (y - center.y) * (y - center.y));
+            if (temp < (radius * radius)) {
                 image1.at<Vec3b>(Point(x, y))[0] = image.at<Vec3b>(Point(x, y))[0];
                 image1.at<Vec3b>(Point(x, y))[1] = image.at<Vec3b>(Point(x, y))[1];
                 image1.at<Vec3b>(Point(x, y))[2] = image.at<Vec3b>(Point(x, y))[2];
-            }
-            else
-            {
+            } else {
                 image2.at<Vec3b>(Point(x, y))[0] = image.at<Vec3b>(Point(x, y))[0];
                 image2.at<Vec3b>(Point(x, y))[1] = image.at<Vec3b>(Point(x, y))[1];
                 image2.at<Vec3b>(Point(x, y))[2] = image.at<Vec3b>(Point(x, y))[2];
