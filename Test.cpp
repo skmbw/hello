0113a806cca39fbe15c7114acfee873809761316
skmbw <tongku2008@126.com>
2020-04-19 14:10:56 +0800

    对代码Sales_item.h进行注释

---
+++ Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yinlei on 17-7-7.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+// Created by yinlei on 17-7-7.
+//
+
+#ifndef HELLO_TEST_H
+#define HELLO_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //HELLO_TEST_H
